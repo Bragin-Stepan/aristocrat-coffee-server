f2e15e71b9e39dd560feee3e756fa021802013c8
Bragin Stepan <braginstepan@gmail.com>
2025-04-23 01:22:12 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 4200
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`);
 
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
   // app.enableShutdownHooks();
-  app.enableCors()
-  await app.listen(process.env.PORT ?? 4200);
+  // app.enableCors()
+  await app.listen(port);
 }
 bootstrap();
