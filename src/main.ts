e13d3b8be92fa438ada10318d73bafc503da3865
Bragin Stepan <braginstepan@gmail.com>
2025-04-23 01:31:00 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  // app.enableShutdownHooks();
-  // app.enableCors()
+  app.enableShutdownHooks();
+  app.enableCors()
   await app.listen(port);
 }
 bootstrap();
