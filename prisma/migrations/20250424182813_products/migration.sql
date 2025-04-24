9e99a331255c42553e199dae6689ea442ab30685
Bragin Stepan <braginstepan@gmail.com>
2025-04-24 23:28:43 +0500

    Обновлены миграции продуктов и категорий

---
+++ prisma/migrations/20250424182813_products/migration.sql
@@ -0,0 +1,72 @@
+/*
+  Warnings:
+
+  - The values [developer] on the enum `roles` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "roles_new" AS ENUM ('user', 'seller', 'admin');
+ALTER TABLE "user" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "user" ALTER COLUMN "role" TYPE "roles_new" USING ("role"::text::"roles_new");
+ALTER TYPE "roles" RENAME TO "roles_old";
+ALTER TYPE "roles_new" RENAME TO "roles";
+DROP TYPE "roles_old";
+ALTER TABLE "user" ALTER COLUMN "role" SET DEFAULT 'user';
+COMMIT;
+
+-- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "components" TEXT NOT NULL,
+    "images" TEXT[],
+    "category_id" TEXT,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_variant" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "price" INTEGER NOT NULL,
+    "size" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+
+    CONSTRAINT "product_variant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "order" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_order_key" ON "product"("order");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_name_key" ON "product"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "category_name_key" ON "category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "category_order_key" ON "category"("order");
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variant" ADD CONSTRAINT "product_variant_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -7,43 +7,60 @@ import { CategoryDto } from './dto/category.dto';
 export class CategoryService {
   constructor(private prisma: PrismaService) {}
 
-  
-	async getAll() {
-		return this.prisma.category.findMany({
-			select: returnCategoryObject
-		})
-	}
+  async getAll() {
+    return this.prisma.category.findMany({
+      select: returnCategoryObject,
+      orderBy: {
+        order: 'asc',
+      },
+    });
+  }
 
-	async create(dto: CategoryDto) {
-    if (dto.role === 'USER') throw new Error('You do not have access to create categories')
 
-		return this.prisma.category.create({
-			data: {
-				name: dto.name,
-			}
-		})
-	}
+  async create(dto: CategoryDto) {
+    if (dto.role === 'USER') throw new Error('You do not have access to create categories');
 
-	async update(id: string, dto: CategoryDto) {
-    if (dto.role === 'USER') throw new Error('You do not have access to update categories')
+    const maxOrder = await this.prisma.category.findFirst({
+      orderBy: { order: 'desc' },
+      select: { order: true },
+    });
 
-		return this.prisma.category.update({
-			where: {
-				id
-			},
-			data: {
-				name: dto.name,
-			}
-		})
-	}
+    const newOrder = maxOrder ? maxOrder.order + 1 : 0; 
 
-	async delete(id: string, dto: CategoryDto) {
-    if (dto.role === 'USER') throw new Error('You do not have access to delete categories')
+    return this.prisma.category.create({
+      data: {
+        name: dto.name,
+        order: newOrder,
+      },
+    });
+  }
 
-		return this.prisma.category.delete({
-			where: {
-				id
-			}
-		})
-	}
+  async update(id: string, dto: CategoryDto) {
+    if (dto.role === 'USER') throw new Error('You do not have access to update categories');
+
+    return this.prisma.category.update({
+      where: { id },
+      data: {
+        name: dto.name,
+      },
+    });
+  }
+
+  async delete(id: string, dto: CategoryDto) {
+    if (dto.role === 'USER') throw new Error('You do not have access to delete categories');
+
+    return this.prisma.category.delete({
+      where: { id },
+    });
+  }
+
+  async updateOrder(ids: string[]) {
+    for (let i = 0; i < ids.length; i++) {
+      const categoryId = ids[i];
+      await this.prisma.category.update({
+        where: { id: categoryId },
+        data: { order: i },
+      });
+    }
+  }
 }
