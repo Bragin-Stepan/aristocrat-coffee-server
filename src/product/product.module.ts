64d3d2ce79b2454391b370669ca72fb7d8e73f3a
Bragin Stepan <braginstepan@gmail.com>
2025-04-28 03:35:45 +0500

    fix

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,14 +1,13 @@
-import { Module } from '@nestjs/common'
-import { CategoryModule } from 'src/category/category.module'
-import { PrismaService } from 'src/prisma.service'
-import { ProductController } from './product.controller'
-import { ProductService } from './product.service'
-import { ImagesService } from 'src/images/images.service'
+import { Module } from '@nestjs/common';
+import { CategoryModule } from 'src/category/category.module';
+import { PrismaService } from 'src/prisma.service';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+import { ImagesService } from 'src/images/images.service';
 
 @Module({
 	controllers: [ProductController],
-	imports: [
-		CategoryModule],
-	providers: [ProductService, PrismaService, ImagesService]
+	imports: [CategoryModule],
+	providers: [ProductService, PrismaService, ImagesService],
 })
 export class ProductModule {}
