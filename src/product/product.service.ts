ebefbe25b465f04a284d3f506521a03d4513e4a7
Bragin Stepan <braginstepan@gmail.com>
2025-04-27 23:48:02 +0500

    Обновлена загрузка картинок

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -7,175 +7,170 @@ import { ImagesService } from 'src/images/images.service';
 
 @Injectable()
 export class ProductService {
-  constructor(
-    private prisma: PrismaService,
-    private categoryService: CategoryService,
-    private readonly imagesService: ImagesService,
-  ) {}
+	constructor(
+		private prisma: PrismaService,
+		private categoryService: CategoryService,
+		private readonly imagesService: ImagesService,
+	) {}
 
-  async getAll(searchTerm?: string) {
-    if (searchTerm) {
-      return this.search(searchTerm);
-    }
+	async getAll(searchTerm?: string) {
+		if (searchTerm) {
+			return this.search(searchTerm);
+		}
 
-    return this.prisma.product.findMany({
-      select: returnProductObject,
-      orderBy: {
-        priority: 'desc',
-      },
-    });
-  }
+		return this.prisma.product.findMany({
+			select: returnProductObject,
+			orderBy: {
+				priority: 'desc',
+			},
+		});
+	}
 
-  async getById(id: string) {
-    const product = await this.prisma.product.findUnique({
-      where: {
-        id,
-      },
-      select: returnProductObject,
-    });
-    if (!product) {
-      throw new NotFoundException('Product not found');
-    }
-    return product;
-  }
+	async getById(id: string) {
+		const product = await this.prisma.product.findUnique({
+			where: {
+				id,
+			},
+			select: returnProductObject,
+		});
+		if (!product) {
+			throw new NotFoundException('Product not found');
+		}
+		return product;
+	}
 
-  async search(searchTerm: string) {
-    return this.prisma.product.findMany({
-      where: {
-        OR: [
-          {
-            name: {
-              contains: searchTerm,
-              mode: 'insensitive',
-            },
-          },
-          {
-            description: {
-              contains: searchTerm,
-              mode: 'insensitive',
-            },
-          },
-        ],
-      },
-      select: returnProductObject,
-    });
-  }
+	async search(searchTerm: string) {
+		return this.prisma.product.findMany({
+			where: {
+				OR: [
+					{
+						name: {
+							contains: searchTerm,
+							mode: 'insensitive',
+						},
+					},
+					{
+						description: {
+							contains: searchTerm,
+							mode: 'insensitive',
+						},
+					},
+				],
+			},
+			select: returnProductObject,
+		});
+	}
 
-  async byCategory(id: string) {
-    const category = await this.categoryService.getById(id);
-    if (!category) {
-      throw new NotFoundException('Category not found');
-    }
-    return this.prisma.product.findMany({
-      where: {
-        categoryId: category.id,
-      },
-      orderBy: {
-        priority: 'desc',
-      },
-      select: returnProductObject,
-    });
-  }
+	async byCategory(id: string) {
+		const category = await this.categoryService.getById(id);
+		if (!category) {
+			throw new NotFoundException('Category not found');
+		}
+		return this.prisma.product.findMany({
+			where: {
+				categoryId: category.id,
+			},
+			orderBy: {
+				priority: 'desc',
+			},
+			select: returnProductObject,
+		});
+	}
 
-  async create(dto: ProductDto) {
-    const maxPriority = await this.prisma.product.findFirst({
-      orderBy: { priority: 'desc' },
-      select: { priority: true },
-    });
+	async create(dto: ProductDto) {
+		const maxPriority = await this.prisma.product.findFirst({
+			orderBy: { priority: 'desc' },
+			select: { priority: true },
+		});
 
-    const newPriority = maxPriority ? maxPriority.priority + 1 : 0;
+		const newPriority = maxPriority ? maxPriority.priority + 1 : 0;
 
-    if (dto.imagesId && dto.imagesId.length > 0) {
-      await Promise.all(
-        dto.imagesId.map(async (imageId) => {
-          const imageExists = await this.imagesService.getImageById(imageId);
-          if (!imageExists) {
-            throw new NotFoundException(`Image with ID ${imageId} not found`);
-          }
-        }),
-      );
-    }
+		if (dto.imagesId && dto.imagesId.length > 0) {
+			await Promise.all(
+				dto.imagesId.map(async imageId => {
+					const imageExists = await this.imagesService.getImageById(imageId);
+					if (!imageExists) {
+						throw new NotFoundException(`Image with ID ${imageId} not found`);
+					}
+				}),
+			);
+		}
 
-    return this.prisma.product.create({
-      data: {
-        name: dto.name,
-        description: dto.description ?? '',
-        components: dto.components ?? '',
-        categoryId: dto.categoryId,
-        priority: newPriority,
-        images: dto.imagesId && dto.imagesId.length > 0 
-          ? { connect: dto.imagesId.map(id => ({ id })) } 
-          : undefined,
-        variants: {
-          create: dto.variants.map((variant) => ({
-            price: variant.price,
-            size: variant.size,
-          })),
-        },
-      },
-      include: {
-        images: true,
-        variants: true,
-      },
-    });
-  }
+		return this.prisma.product.create({
+			data: {
+				name: dto.name,
+				description: dto.description ?? '',
+				components: dto.components ?? '',
+				categoryId: dto.categoryId,
+				priority: newPriority,
+				images:
+					dto.imagesId && dto.imagesId.length > 0
+						? { connect: dto.imagesId.map(id => ({ id })) }
+						: undefined,
+				variants: {
+					create: dto.variants.map(variant => ({
+						price: variant.price,
+						size: variant.size,
+					})),
+				},
+			},
+			include: {
+				images: true,
+				variants: true,
+			},
+		});
+	}
 
-  async update(id: string, dto: ProductDto) {
-    const product = await this.getById(id);
-    if (!product) {
-      throw new NotFoundException('Product not found');
-    }
+	async update(id: string, dto: ProductDto) {
+		const product = await this.getById(id);
+		if (!product) {
+			throw new NotFoundException('Product not found');
+		}
 
-    await this.categoryService.getById(dto.categoryId);
+		await this.categoryService.getById(dto.categoryId);
 
-    if (dto.imagesId && dto.imagesId.length > 0) {
-      await Promise.all(
-        dto.imagesId.map(async (imageId) => {
-          const imageExists = await this.imagesService.getImageById(imageId);
-          if (!imageExists) {
-            throw new NotFoundException(`Image with ID ${imageId} not found`);
-          }
-        }),
-      );
-    }
+		if (dto.imagesId && dto.imagesId.length > 0) {
+			await Promise.all(
+				dto.imagesId.map(async imageId => {
+					const imageExists = await this.imagesService.getImageById(imageId);
+					if (!imageExists) {
+						throw new NotFoundException(`Image with ID ${imageId} not found`);
+					}
+				}),
+			);
+		}
 
-    return this.prisma.product.update({
-      where: { id },
-      data: {
-        name: dto.name,
-        description: dto.description,
-        components: dto.components,
-        categoryId: dto.categoryId,
-        images: dto.imagesId 
-          ? { 
-              set: [],
-              connect: dto.imagesId.map(id => ({ id }))
-            } 
-          : undefined,
-        variants: {
-          deleteMany: {},
-          create: dto.variants.map((variant) => ({
-            price: variant.price,
-            size: variant.size,
-          })),
-        },
-      },
-      include: {
-        images: true,
-        variants: true,
-      },
-    });
-  }
+		return this.prisma.product.update({
+			where: { id },
+			data: {
+				name: dto.name,
+				images: {
+					set: dto.imagesId.map(id => ({ id })),
+				},
+				variants: {
+					deleteMany: {},
+					create: dto.variants.map(variant => ({
+						price: variant.price,
+						size: variant.size,
+					})),
+				},
+			},
+			include: {
+				images: true,
+				variants: true,
+			},
+		});
+	}
 
-  async delete(id: string) {
-    return this.prisma.$transaction(async (prisma) => {
-      await prisma.productVariant.deleteMany({
-        where: { productId: id },
-      });
+	async delete(id: string) {
+		return this.prisma.$transaction(async prisma => {
+			await prisma.productVariant.deleteMany({
+				where: { productId: id },
+			});
 
-      await prisma.product.delete({
-        where: { id },
-      });
-    });
-  }
+			await prisma.product.delete({
+				where: { id },
+			});
+		});
+	}
 }
